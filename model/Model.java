a124e446e3ffd9b8e7c52ff15bdd2858bae3bb17
monykansaramk <35912472+monykansaramk@users.noreply.github.com>
2019-03-20 12:28:59 +0530

    Add files via upload

---
+++ model/Model.java
@@ -0,0 +1,81 @@
+package model;
+
+public class Model {
+
+	private String uname;
+	private String password;
+	private String cpassword;
+	private String name;
+	private String email;
+	private String contact_no;
+	private String latitude;
+	private String longitude;
+	private String area;
+	private String offer_name;
+	
+	
+	
+	
+	public String getOffer_name() {
+		return offer_name;
+	}
+	public void setOffer_name(String offer_name) {
+		this.offer_name = offer_name;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCpassword() {
+		return cpassword;
+	}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(String contact_no) {
+		this.contact_no = contact_no;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	
+}
